d36e7e7410f9b7d50b0a3943a7c7a7de8d82093d
marcos_nielsen <marcos@softniels.com.br>
2020-02-20 10:25:22 -0300

    conversao para androidx

--- src/ForegroundPlugin.java
+++ src/ForegroundPlugin.java
@@ -1,44 +1,40 @@
 package com.softniels.foregroundservicex;
 
-import android.app.Activity;
-import android.content.Intent;
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.annotation.TargetApi;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class ForegroundPlugin extends CordovaPlugin {
+
     @Override
-    @TargetApi(26)
-    public boolean execute (final String action, final JSONArray args, final CallbackContext command) throws JSONException {
-        if (android.os.Build.VERSION.SDK_INT >= 26) {
-            Activity activity = cordova.getActivity();
-            Intent intent = new Intent(activity, ForegroundService.class);
-
-            if (action.equals("start")) {
-                // Tell the service we want to start it
-                intent.setAction("start");
-
-                // Pass the notification title/text/icon to the service
-                intent.putExtra("title", args.getString(0))
-                    .putExtra("text", args.getString(1))
-                    .putExtra("icon", args.getString(2))
-                    .putExtra("importance", args.getString(3))
-                    .putExtra("id", args.getString(4));
-                
-                // Start the service
-                activity.getApplicationContext().startForegroundService(intent);
-            } else if (action.equals("stop")) {
-                // Tell the service we want to stop it
-                intent.setAction("stop");
-
-                // Stop the service
-                activity.getApplicationContext().startService(intent);
-            }
+    public boolean execute(final String action, final JSONArray args, final CallbackContext command)
+            throws JSONException {
+        if (action.equals("start")) {
+            startService();
+        } else if (action.equals("stop")) {
+            stopService();
         }
-
         command.success();
         return true;
+    }
+
+    private void startService() {
+        Context context = getApplicationContext();
+        Activity activity = cordova.getActivity();
+        Intent serviceIntent = new Intent(activity, ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android");
+        ContextCompat.startForegroundService(context, serviceIntent);
+    }
+
+    private void stopService() {
+        Activity activity = cordova.getActivity();
+        Intent serviceIntent = new Intent(activity, ForegroundService.class);
+        stopService(serviceIntent);
     }
 }
--- src/ForegroundService.java
+++ src/ForegroundService.java
@@ -1,94 +1,66 @@
 package com.softniels.foregroundservicex;
 
-import android.content.Intent;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.app.Service;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.app.Service;
+
+import android.os.Build;
 import android.os.IBinder;
-import android.os.Bundle;
-import android.annotation.TargetApi;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class ForegroundService extends Service {
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getAction().equals("start")) {
-            // Start the service
-            startPluginForegroundService(intent.getExtras());
-        } else {
-            // Stop the service
-            stopForeground(true);
-            stopSelf();
-        }
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
 
-        return START_STICKY;
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 
-    @TargetApi(26)
-    private void startPluginForegroundService(Bundle extras) {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Context context = getApplicationContext();
-
-        // Delete notification channel if it already exists
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.deleteNotificationChannel("foreground.service.channel");
-
-        // Get notification channel importance
-        Integer importance;
-
-        try {
-            importance = Integer.parseInt((String) extras.get("importance"));
-        } catch (NumberFormatException e) {
-            importance = 1;
-        }
-
-        switch(importance) {
-            case 2:
-                importance = NotificationManager.IMPORTANCE_DEFAULT;
-                break;
-            case 3:
-                importance = NotificationManager.IMPORTANCE_HIGH;
-                break;
-            default:
-                importance = NotificationManager.IMPORTANCE_LOW;
-            // We are not using IMPORTANCE_MIN because we want the notification to be visible
-        }
-
-        // Create notification channel
-        NotificationChannel channel = new NotificationChannel("foreground.service.channel", "Background Services", importance);
-        channel.setDescription("Enables background processing.");
-        getSystemService(NotificationManager.class).createNotificationChannel(channel);
-
-        // Get notification icon
-        int icon = getResources().getIdentifier((String) extras.get("icon"), "drawable", context.getPackageName());
-        //cria a pendingIntent para ser aberta quando clicar no link
+        String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
         Intent notificationIntent = new Intent(ForegroundService.this, io.cordova.hellocordova.MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(ForegroundService.this,
                 0, notificationIntent, 0);
-        // Make notification
-        Notification notification = new Notification.Builder(context, "foreground.service.channel")
-            .setContentTitle((CharSequence) extras.get("title"))
-            .setContentText((CharSequence) extras.get("text"))
-            .setOngoing(true)
-            .setSmallIcon(icon == 0 ? 17301514 : icon) // Default is the star icon
-            .setContentIntent(pendingIntent)
-            .build();
-
-        // Get notification ID
-        Integer id;
-        try {
-            id = Integer.parseInt((String) extras.get("id"));
-        } catch (NumberFormatException e) {
-            id = 0;
-        }
-
-        // Put service in foreground and show notification (id of 0 is not allowed)
-        startForeground(id != 0 ? id : 197812504, notification);
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle("Application")
+                .setContentText(input)
+                //.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+        //do heavy work on a background thread
+        //stopSelf();
+        return START_NOT_STICKY;
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
     public IBinder onBind(Intent intent) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
     }
 }
